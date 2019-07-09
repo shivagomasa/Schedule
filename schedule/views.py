cc3031d87d66377bf05a557b178c1712ab15a8d2
shivagomasa <shivagomasa@gmail.com>
2019-07-09 18:56:42 +0530

    Add ability to view list on homepage.

--- schedule/views.py
+++ schedule/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Task
 
 # Create your views here.
+
+class TaskList(ListView):
+    model = Task
+    template_name = 'schedule/task_list.html'
