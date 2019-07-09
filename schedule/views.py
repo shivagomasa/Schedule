dc35eaae31b81824c2d2742844e6af3e2040443c
shivagomasa <shivagomasa@gmail.com>
2019-07-09 19:22:22 +0530

    Add ability to view detail of each task.

--- schedule/views.py
+++ schedule/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView,DetailView
 from .models import Task
 
 # Create your views here.
@@ -7,3 +7,7 @@ from .models import Task
 class TaskList(ListView):
     model = Task
     template_name = 'schedule/task_list.html'
+
+class TaskDetail(DetailView):
+    model = Task
+    template_name = 'schedule/task_detail.html'
