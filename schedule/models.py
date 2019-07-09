0666e7171392ea55968e600f39c66309f7aeabea
shivagomasa <shivagomasa@gmail.com>
2019-07-09 18:47:48 +0530

    Create a model and register in admin interface.

--- schedule/models.py
+++ schedule/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=70)
+    date = models.DateField(blank=True,null=True)
+    time = models.TimeField(blank=True,null=True)
+    created = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
