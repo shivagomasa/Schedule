71d64ed9931dbba2dca185991be65909c6b30fc0
shivagomasa <shivagomasa@gmail.com>
2019-07-12 10:58:30 +0530

    Add colors to project.

--- Remainder/settings.py
+++ Remainder/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Application definition
 
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'schedule',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
